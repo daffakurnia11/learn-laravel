5fcea931dd2661471802475d91ef98c606494f83
daffakurnia11 <daffakurniaf11@gmail.com>
2021-03-17 00:35:59 +0700

    Migrating Table to Database
    Create Migrating Table named "Students" using command : php artisan make:migration create_students_table
    Add new database named "learn_laravel"
    Migrate the table using command "php artisan migrate"

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.main')
+
+@section('title', 'Student Lists')
+
+@section('container')
+
+<div class="mt-3 container">
+  <div class="row">
+    <div class="col-sm-6">
+      <h1>Student Lists</h1>
+      <ul class="list-group">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          A list item
+          <span class="badge badge-primary badge-pill">14</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+@endsection
